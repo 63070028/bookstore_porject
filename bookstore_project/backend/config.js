d7c35eaf233ee554443255b1fc2b80a5181e6e3b
chanapon chuenchuwit <71110346+63070028@users.noreply.github.com>
2023-01-23 21:48:50 +0700

    Update config.js

--- bookstore_project/backend/config.js
+++ bookstore_project/backend/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Kong041300',
+  password: 'xxxxxxxx',
   database: 'bookstore',
   waitForConnections: true,
   connectionLimit: 10,
